ce22758b6e6809a8b70326d0ce11dc8c5b9d564e
Alena Golovaneva <a.golovaneva@innoloft.com>
2021-09-20 11:08:55 +0200

    Add user and movie models

---
+++ models/movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: () => 'Movie image link is not valid',
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: () => 'Trailer link is not valid',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: () => 'Thumbnail link is not valid',
+    },
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    requried: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: mongoose.Types.ObjectId,
+    requried: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEn: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: () => 'User email is not valid',
+    },
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
