1f68a5eaace778628a4e36cebe31a70e789e690c
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-09-21 20:24:47 +0200

    Change db name

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const {
   JWT_SECRET = '123',
   NODE_ENV = 'development',
-  MONGO_URL = 'mongodb://localhost:27017/movies-explorer',
+  MONGO_URL = 'mongodb://localhost:27017/moviesdb',
   PORT = '3000',
 } = process.env;
 
